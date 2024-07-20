74d92b8ee34fd5c344435e24d1cd04d489d2cc22
Gregory <gregory.hue135@gmail.com>
2024-07-20 17:38:33 +0200

    Gathering all data

--- app/app/pipelines.py
+++ app/app/pipelines.py
@@ -5,9 +5,9 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
 
 
 class AppPipeline:
     def process_item(self, item, spider):
+        print(item)
         return item
