64feed8a3b4531d26bfbe96ddf4146a0d75a88b5
Your Name <gregory.hue135@gmail.com>
2024-08-13 08:46:23 +0200

    ALLOWED_HOSTS for render

--- main/main/settings.py
+++ main/main/settings.py
@@ -33,6 +33,9 @@ DEBUG = False if os.getenv("ENV") == "prod" else True
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
